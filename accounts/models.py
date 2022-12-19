6c16dc2a8fd3c6dda47cd1baf045d14968d92ef5
abdo1fraza <abdo87.hamza@gmail.com>
2022-12-19 07:49:05 +0200

    add create profile signal

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # class Profile to complete user sign up information
 
@@ -13,7 +15,14 @@ class Profile(models.Model):
     block_email = models.CharField(max_length = 15, unique = True)
 
 
-    def __str__(self) -> str:
+    def __str__(self):
         return str(self.user)
      
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kargw):
+    if created:
+        Profile.objects.create(
+            user = instance
+        )
 
